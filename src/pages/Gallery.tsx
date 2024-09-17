6ecfb0add5f01666f5f1f2915c9ef94b993bb5be
edita <edignot@gmail.com>
2024-09-16 19:53:56 -0600

    debug actions in gallery

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,3 +1,4 @@
+// import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState, AppDispatch } from '../state/store'
 import {
@@ -16,10 +17,14 @@ const Gallery = () => {
         (state: RootState) => state.gallery
     )
 
+    // useEffect(() => {
+    //     console.log({ photos, selectedPhoto, loading, error })
+    // }, [photos, selectedPhoto, loading, error])
+
     const handleGetPhotos = async () => {
         try {
             const response = await dispatch(getPhotos())
-            console.log('Photos', response)
+            // console.log('Photos', response)
         } catch (error) {
             console.error('Error getting photos:', error)
         }
@@ -28,7 +33,7 @@ const Gallery = () => {
     const handleGetPhoto = async () => {
         try {
             const response = await dispatch(
-                getPhoto('66e7754974319f510e39d50b')
+                getPhoto('66e77c520914d0ff29198bd3')
             )
             console.log('Photo', response)
         } catch (error) {
@@ -36,55 +41,55 @@ const Gallery = () => {
         }
     }
 
-    const handleSelectPhoto = () => {
-        dispatch(selectPhoto('66e7754974319f510e39d50b'))
-        console.log('Selected photo', selectedPhoto)
-    }
+    // const handleSelectPhoto = () => {
+    //     dispatch(selectPhoto('test'))
+    // }
 
-    const handleCreatePhoto = async () => {
-        try {
-            const response = await dispatch(
-                createPhoto({ photoUrl: 'test', note: 'test' })
-            )
-            console.log('Created photo', response)
-        } catch (error) {
-            console.error('Error creating photo:', error)
-        }
-    }
+    // const handleCreatePhoto = async () => {
+    //     try {
+    //         const response = await dispatch(
+    //             createPhoto({ photoUrl: 'test', note: 'test' })
+    //         )
+    //         console.log('Created photo', response)
+    //     } catch (error) {
+    //         console.error('Error creating photo:', error)
+    //     }
+    // }
 
-    const handleUpdatePhoto = async () => {
-        try {
-            const response = await dispatch(
-                updatePhoto({
-                    photoId: '66e7754974319f510e39d50b',
-                    note: 'test',
-                })
-            )
-            console.log('Updated photo', response)
-        } catch (error) {
-            console.error('Error updating photo:', error)
-        }
-    }
+    // const handleUpdatePhoto = async () => {
+    //     try {
+    //         const response = await dispatch(
+    //             updatePhoto({
+    //                 photoId: '66e77c520914d0ff29198bd3',
+    //                 note: 'UPDATED',
+    //             })
+    //         )
+    //         console.log('Updated photo', response)
+    //     } catch (error) {
+    //         console.error('Error updating photo:', error)
+    //     }
+    // }
 
-    const handleDeletePhoto = async () => {
-        try {
-            const response = await dispatch(
-                deletePhoto('66e7754974319f510e39d50b')
-            )
-            console.log('Deleted photo', response)
-        } catch (error) {
-            console.error('Error deleting photo:', error)
-        }
-    }
+    // // state does not update when deleted
+    // const handleDeletePhoto = async () => {
+    //     try {
+    //         const response = await dispatch(
+    //             deletePhoto('66e8d6eae34eedbd97ba308a')
+    //         )
+    //         console.log('Deleted photo', response)
+    //     } catch (error) {
+    //         console.error('Error deleting photo:', error)
+    //     }
+    // }
 
     return (
         <div>
             <button onClick={handleGetPhotos}>Get Photos</button>
             <button onClick={handleGetPhoto}>Get Photo</button>
-            <button onClick={handleSelectPhoto}>Select Photo</button>
+            {/* <button onClick={handleSelectPhoto}>Select Photo</button>
             <button onClick={handleCreatePhoto}>Create Photo</button>
             <button onClick={handleUpdatePhoto}>Update Photo</button>
-            <button onClick={handleDeletePhoto}>Delete Photo</button>
+            <button onClick={handleDeletePhoto}>Delete Photo</button> */}
             {loading && <p>Loading photos...</p>}
             {error && <p>Error fetching photos!</p>}
             {photos.length > 0 && (
